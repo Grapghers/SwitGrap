802fca3df7d8f13facb82a79c6838814f13a85c9
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-13 19:41:17 +0900

    [refactoring] #1 - Package

--- Package.swift
+++ Package.swift
@@ -9,18 +9,14 @@ let package = Package(
         .macOS(.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwitGrap",
-            targets: ["SwitGrap"]),
+        .executable(name: "SwitGrap", targets: ["SwitGrap"]),
+        .library(name: "SwitGrapModuleImportPlugin", type: .dynamic, targets: ["SwitGrapModuleImportPlugin"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/swit-graph/SwitGrapPluginSupport", .upToNextMinor(from: "0.0.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwitGrap",
             dependencies: [
