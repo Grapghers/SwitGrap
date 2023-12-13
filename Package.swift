242843c80e861c48a15af0378a6974dd64eea340
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-13 17:20:16 +0900

    [+] #1 - Add dependency

--- Package.swift
+++ Package.swift
@@ -5,19 +5,47 @@ import PackageDescription
 
 let package = Package(
     name: "SwitGrap",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwitGrap",
             targets: ["SwitGrap"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.0")),
+        .package(url: "https://github.com/swit-graph/SwitGrapPluginSupport", .upToNextMinor(from: "0.0.1"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwitGrap"),
+            name: "SwitGrap",
+            dependencies: [
+                "SwitGrapLib",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        .target(
+            name: "SwitGrapLib",
+            dependencies: [
+                "SwitGrapPluginSupport",
+            ]
+        ),
+        .target(
+            name: "SwitGrapModuleImportPlugin",
+            dependencies: [
+                "SwitGrapPluginSupport",
+            ]
+        ),
+        
         .testTarget(
             name: "SwitGrapTests",
             dependencies: ["SwitGrap"]),
+        
+        .testTarget(name: "SwitGrapLibTests",
+                   dependencies: ["SwitGrapLib"])
     ]
 )
